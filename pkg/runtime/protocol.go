a88686780364b1429f1f9eccd669c3346b7ee392
antonionduarte <an.duarte@campus.fct.unl.pt>
2023-10-21 17:48:54 +0000

    added Receive and Send functions in ProtoProtocol.

--- pkg/runtime/protocol.go
+++ pkg/runtime/protocol.go
@@ -1,12 +1,16 @@
 package runtime
 
-import "bytes"
+import (
+	"bytes"
+	"github.com/antonionduarte/go-simple-protocol-runtime/pkg/runtime/net"
+	"strconv"
+)
 
 type (
 	Protocol interface {
-		Start()                 // Start the protocol, and register all the message handlers
-		Init()                  // Init the protocol, runs after all protocols are registered, send initial messages here
-		ProtocolID() ProtocolID // Returns the protocol ID
+		Start()          // Start the protocol, and register all the message handlers
+		Init()           // Init the protocol, runs after all protocols are registered, send initial messages here
+		ProtocolID() int // Returns the protocol ID
 	}
 
 	ProtoProtocol struct {
@@ -16,21 +20,25 @@ type (
 		messageChannel chan Message
 		bufferChannel  chan bytes.Buffer
 
-		msgSerializers map[MessageID]func(buffer bytes.Buffer) (msg Message)
-		msgHandlers    map[MessageID]func(msg Message) // TODO: this is never going to receive a Message, it's going to still receive a Buffer
-		timerHandlers  map[TimerID]func(timer Timer)
-	}
+		msgSerializers map[int]Serializer
 
-	ProtocolID int
+		msgHandlers   map[int]func(msg Message) // TODO: this is never going to receive a Message, it's going to still receive a Buffer
+		timerHandlers map[TimerID]func(timer Timer)
+	}
 )
 
 func NewProtoProtocol(protocol Protocol) *ProtoProtocol {
 	return &ProtoProtocol{
-		protocol:       protocol,
+		protocol: protocol,
+
+		msgSerializers: make(map[int]Serializer),
+
 		messageChannel: make(chan Message),
 		timerChannel:   make(chan Timer),
-		msgHandlers:    make(map[MessageID]func(msg Message)),
-		timerHandlers:  make(map[TimerID]func(timer Timer)),
+		bufferChannel:  make(chan bytes.Buffer),
+
+		msgHandlers:   make(map[int]func(msg Message)),
+		timerHandlers: make(map[TimerID]func(timer Timer)),
 	}
 }
 
@@ -65,7 +73,7 @@ func (p *ProtoProtocol) EventHandler() {
 	}
 }
 
-func (p *ProtoProtocol) ProtocolID() ProtocolID {
+func (p *ProtoProtocol) ProtocolID() int {
 	return p.protocol.ProtocolID()
 }
 
@@ -75,4 +83,46 @@ func (p *ProtoProtocol) TimerChannel() chan Timer {
 
 func (p *ProtoProtocol) MessageChannel() chan Message {
 	return p.messageChannel
+}
+
+// SendMessage sends a message to a host via the Network Layer.
+func SendMessage(msg Message, host *net.Host) {
+	msgBuffer, err := msg.Serializer().Serialize()
+
+	if err != nil {
+		// TODO: Replace with decent logger event.
+	}
+
+	// Add to the Buffer: [ProtocolID][MessageID][Message]
+	protocolIDBytes := []byte(strconv.Itoa(msg.ProtocolID()))
+	messageIDBytes := []byte(strconv.Itoa(msg.MessageID()))
+
+	// Create a buffer and write the bytes.
+	buffer := new(bytes.Buffer)
+	buffer.Write(protocolIDBytes)
+	buffer.Write(messageIDBytes)
+	buffer.Write(msgBuffer.Bytes())
+
+	GetRuntimeInstance().networkLayer.Send(buffer, host)
+}
+
+// ReceiveMessage receives a message from the Network Layer.
+func ReceiveMessage(buffer bytes.Buffer) {
+	protocolIDByte := buffer.Next(1)
+	messageIDByte := buffer.Next(1)
+
+	protocolID, err := strconv.Atoi(string(protocolIDByte))
+	if err != nil {
+		// TODO: Replace with decent logger event.
+	}
+
+	messageID, err := strconv.Atoi(string(messageIDByte))
+	if err != nil {
+		// TODO: Replace with decent logger event.
+	}
+
+	protocol := GetRuntimeInstance().protocols[protocolID]
+	message, _ := protocol.msgSerializers[messageID].Deserialize(buffer)
+
+	protocol.messageChannel <- message
 }

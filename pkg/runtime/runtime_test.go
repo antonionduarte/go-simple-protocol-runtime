9613f2a9b13d3a2121120cd1bf359499d904cda6
antonionduarte <an.duarte@campus.fct.unl.pt>
2023-11-20 01:25:16 +0000

    added basic tests to runtime_test.go

---
+++ pkg/runtime/runtime_test.go
@@ -0,0 +1,96 @@
+package runtime
+
+import (
+	"github.com/antonionduarte/go-simple-protocol-runtime/pkg/runtime/net"
+	"testing"
+)
+
+type MockProtocol struct {
+	StartCalled bool
+	InitCalled  bool
+	ProtoID     int
+	MockSelf    *net.Host
+}
+
+func (m *MockProtocol) Start() {
+	m.StartCalled = true
+}
+
+func (m *MockProtocol) Init() {
+	m.InitCalled = true
+}
+
+func (m *MockProtocol) ProtocolID() int {
+	return m.ProtoID
+}
+
+func (m *MockProtocol) Self() *net.Host {
+	return m.MockSelf
+}
+
+type MockNetworkLayer struct {
+	ConnectCalled    bool
+	DisconnectCalled bool
+	SendCalled       bool
+	CancelCalled     bool
+	StartCalled      bool
+}
+
+func (m *MockNetworkLayer) Connect(host *net.Host) {
+	m.ConnectCalled = true
+}
+
+func (m *MockNetworkLayer) Disconnect(host *net.Host) {
+	m.DisconnectCalled = true
+}
+
+func (m *MockNetworkLayer) Send(msg *net.NetworkMessage) {
+	m.SendCalled = true
+}
+
+func (m *MockNetworkLayer) OutChannel() chan *net.NetworkMessage {
+	return make(chan *net.NetworkMessage, 1)
+}
+
+func (m *MockNetworkLayer) OutChannelEvents() chan net.ConnEvents {
+	return make(chan net.ConnEvents, 1)
+}
+
+func (m *MockNetworkLayer) Cancel() {
+	m.CancelCalled = true
+}
+
+func TestGetRuntimeInstance(t *testing.T) {
+	instance1 := GetRuntimeInstance()
+	instance2 := GetRuntimeInstance()
+
+	if instance1 != instance2 {
+		t.Errorf("GetRuntimeInstance should return the same instance")
+	}
+}
+
+func TestRegisterProtocol(t *testing.T) {
+	runtime := GetRuntimeInstance()
+	mockProtocol := &MockProtocol{ProtoID: 123}
+	testHost := net.NewHost(8080, "127.0.0.1") // Create a Host instance for testing
+
+	protoProtocol := NewProtoProtocol(mockProtocol, testHost)
+
+	runtime.RegisterProtocol(protoProtocol)
+
+	if _, exists := runtime.protocols[mockProtocol.ProtocolID()]; !exists {
+		t.Errorf("Protocol was not registered correctly")
+	}
+}
+
+func TestStartAndCancel(t *testing.T) {
+	runtime := GetRuntimeInstance()
+	mockNetworkLayer := new(MockNetworkLayer)
+	runtime.RegisterNetworkLayer(mockNetworkLayer)
+
+	runtime.Start()
+	runtime.Cancel()
+	if !mockNetworkLayer.CancelCalled {
+		t.Errorf("Expected Cancel to be called on network layer, but it wasn't")
+	}
+}

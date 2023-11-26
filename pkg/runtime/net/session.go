8c9983ca2e8731fc8310ade1b3a6d9545f967eea
antonionduarte <an.duarte@campus.fct.unl.pt>
2023-11-26 02:30:09 +0000

    beggining of session layer

--- pkg/runtime/net/session.go
+++ pkg/runtime/net/session.go
@@ -1,18 +1,24 @@
 package net
 
-import "bytes"
+import (
+	"bytes"
+	"context"
+)
 
 type (
 	SessionLayer struct {
-		transport        TransportLayer // the transport layer used, could be TCP, UDP, QUIC.
-		serverMappings   map[Host]Host  // mapping of Client Addr to Server Addr.
+		connectChan      chan Host
+		disconnectChan   chan Host
+		sendChan         chan SessionMessage
+		transport        TransportLayer
+		serverMappings   map[TransportHost]Host // mapping of Client Addr to Server Addr.
 		outChannelEvents chan TransportConnEvents
 		outMessages      chan SessionMessage
 	}
 
 	SessionMessage struct {
-		Host Host // server host of each peer instead of the client host that the transport layer receives
-		Msg  bytes.Buffer
+		host Host // server host of each peer instead of the client host that the transport layer receives
+		msg  bytes.Buffer
 	}
 
 	ConnEvents int
@@ -24,21 +30,78 @@ const (
 	ConnFailed
 )
 
-func NewNetworkLayer(transport *TransportLayer) *SessionLayer {
-	return nil
+func NewSessionLayer(transport TransportLayer, ctx context.Context) *SessionLayer {
+	session := &SessionLayer{
+		connectChan:      make(chan Host),
+		disconnectChan:   make(chan Host),
+		sendChan:         make(chan SessionMessage),
+		outChannelEvents: make(chan TransportConnEvents),
+		outMessages:      make(chan SessionMessage),
+		serverMappings:   make(map[TransportHost]Host),
+		transport:        transport,
+	}
+	go session.handler(ctx) // TODO: Rethink this
+	return session
 }
 
-func (n *SessionLayer) sessionHandler() { // TODO: Contexts and WaitGroups.
+func (s *SessionLayer) Connect(host Host) {
+	s.connectChan <- host
+}
+
+func (s *SessionLayer) Disconnect(host Host) {
+	s.disconnectChan <- host
+}
+
+func (s *SessionLayer) Send(msg bytes.Buffer, sendTo Host) {
+	s.sendChan <- SessionMessage{msg: msg, host: sendTo}
+}
+
+func (s *SessionLayer) handler(ctx context.Context) {
 	for {
 		select {
-		case event := <-n.transport.OutChannelEvents():
-			n.outChannelEvents <- event
-		case msg := <-n.transport.OutChannel():
-			n.processMessage(msg)
+		case <-ctx.Done():
+			return
+		case host := <-s.connectChan:
+			s.connect(host)
+		case host := <-s.disconnectChan:
+			s.disconnect(host)
+		case msg := <-s.sendChan:
+			s.send(msg.msg, msg.host)
 		}
 	}
 }
 
-func (n *SessionLayer) processMessage(msg TransportMessage) {
+/*
+Problems (off the top of my head):
+	-	When someone connects to us instead of the opposite,
+		we also need to propagate that info, with the respective ServerHost
+		to this layer.
 
+	-	All the ConnEvents (both the ones we propagate up and the ones we receive)
+		from the lower layer, need to be richer - contain the TransportHost that they involve.
+*/
+
+func (s *SessionLayer) connect(host Host) {
+	// this requires an entire protocol...
+}
+
+func (s *SessionLayer) handshake() {
+	// protocol:
+	// - connect via TCP to remote host
+	//	- send session message asking for ServerHost
+	//	- recv session message with reply about ServerHost
+	//	- save ServerHost in TransportHost->ServerHost and ServerHost->TransportHost map
+	//	- send ConnConnected event to the up layers
+}
+
+func (s *SessionLayer) disconnect(host Host) {
+	// s.transport.Disconnect() TODO: Gotta think about which host to use here
+	// - attempt to get TransportHost from ServerHost->TransportHost map
+	// - disconnect from that TransportHost via the TCPLayer
+}
+
+func (s *SessionLayer) send(msg bytes.Buffer, sendTo Host) {
+	// s.transport.Send() TODO: Gotta think about which host to use here
+	// - attempt to get TransportHost from ServerHost->TransportHost map
+	// - send message to that TransportHost via the TCPLayer
 }
